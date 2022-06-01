d1d77bb7180a68ec1b8955c6c6c72e44a8e336f4
antonfastcomet <43189720+antonfastcomet@users.noreply.github.com>
2022-06-01 10:02:24 +0300

    Create repo.sh
    Bash script for correcting CentOS 6 base repositories in case of 404 Not Found errors during yum installations

---
+++ repo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Check if the user is logged in as root
+if [[ $EUID -ne 0 ]]; then
+echo -e "\e[41mYou need to be logged in as the root user.\e[0m"
+exit 1
+else
+#Check CentOS version
+if rpm -q centos-release | grep -i "el6" > /dev/null; then
+echo -e "\e[42mChecking CentOS 6 base repos\e[0m"
+#Check if CentOS 6 base repo is correct
+if grep -q "6.10" /etc/yum.repos.d/CentOS-Base.repo > /dev/null && grep -i "/x86_64" /etc/yum.repos.d/CentOS-Base.repo > /dev/null;  then
+echo -e "\e[42mBase repos already configured\e[0m"
+else
+rm -rf /etc/yum.repos.d/CentOS-Base.repo
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://antonrcc.depro7.fcomet.com/CentOS-Base.repo > /dev/null
+echo -e "\e[42mBase repo URLs have been modified\e[0m"
+fi
+fi
+fi
